11275d1724b0204f2a50237ae8b51a8abf7d4ef3
CodeGuruAmal <amalramesh916@gmail.com>
2024-09-04 15:10:51 +0530

    add: localStorage of cartData, addedItems, clear, removeItems. Now remaining placeId

--- src/utils/cartSlice.js
+++ src/utils/cartSlice.js
@@ -3,26 +3,37 @@ import { createSlice } from "@reduxjs/toolkit";
 const cartSlice = createSlice({
   name: "cart",
   initialState: {
-    cartData: [],
-    addedItem: [],
+    cartData: JSON.parse(localStorage.getItem('cartData')) || [],
+    addedItem: JSON.parse(localStorage.getItem('addedItem')) || [],
   },
   reducers: {
     setCartData: (state, action) => {
       state.cartData = [...state.cartData, action.payload];
+
+      localStorage.setItem('cartData', JSON.stringify(state.cartData));
     },
 
     setAddedItem: (state, action) => {
       state.addedItem = [...state.addedItem, action.payload];
+      
+      localStorage.setItem('addedItem', JSON.stringify(state.addedItem));
     },
 
     clearCart: (state, action) => {
       state.cartData = [];
       state.addedItem = [];
 
+      localStorage.removeItem('cartData');
+      localStorage.removeItem('addedItem');
+
     },
     removeItem: (state, action) => {
       state.cartData = state.cartData.filter(item => item.id !== action.payload);
-      state.addedItem = state.addedItem.filter(item => item.id !== action.payload);
+      state.addedItem = state.addedItem.filter(item => item !== action.payload);
+
+      localStorage.setItem('cartData', JSON.stringify(state.cartData));
+      localStorage.setItem('addedItem', JSON.stringify(state.addedItem));
+
     },
   },
 });
